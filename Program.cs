1967458ba42844890db0e39bcd7c91eca31a95c5
yilmaz.tasdelen <yilmaztasdelen1994@gmail.com>
2024-09-29 22:37:27 +0300

    Azure_Entra_id_MSAL_simple_login

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+
+
+namespace az204_auth
+{
+    class Program
+    {
+        private const string _clientId = "APPLICATION_CLIENT_ID";
+        private const string _tenantId = "DIRECTORY_TENANT_ID";
+        
+
+        public static async Task Main(string[] args)
+        {
+            string[] scopes = { "user.read" };
+            // build out the authorization context.
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build();
+
+            //request the token and write the result out to the console.
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+
+        }
+
+    }
+}
